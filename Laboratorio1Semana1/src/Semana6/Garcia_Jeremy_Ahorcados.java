2ec9cd240fc73bfcd1e011ce97cddaeb954e06bf
Jezerav <jeremy.robateau09@gmail.com>
2025-05-30 16:29:42 -0600

    Agregue los frames
    Agregue la parte del Menu Principal, el juego principal, y solo el diseño de cambia rpalabras

--- Laboratorio1Semana1/src/Semana6/Garcia_Jeremy_Ahorcados.java
+++ Laboratorio1Semana1/src/Semana6/Garcia_Jeremy_Ahorcados.java
@@ -1,13 +1,224 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package Semana6;
 
-/**
- *
- * @author jerem
- */
-public class Garcia_Jeremy_Ahorcados {
+import java.util.Random;
+import javax.swing.JOptionPane;
+
+public class Garcia_Jeremy_Ahorcados extends javax.swing.JFrame {
     
+    private Garcia_Jeremy_Menu menuPrincipal;
+    private String[] palabras = {"PIZZA", "LAPTOP", "OVEJA", "GATO", "MANZANA", "HAMBURGUESA", "FELIZ", "FUTBOL", "MUSICA", "HEROE"};
+    private String palabraSeleccionada;
+    private char[] letrasAdivinadas;
+    private int oportunidades = 5;
+    
+    public Garcia_Jeremy_Ahorcados(Garcia_Jeremy_Menu menu, String[] palabras) {
+        this.menuPrincipal = menu;
+        initComponents();
+        iniciarJuego();
+    }
+
+    public void iniciarJuego() {
+        Random rand = new Random();
+        palabraSeleccionada = palabras[rand.nextInt(palabras.length)].toUpperCase(); 
+        letrasAdivinadas = new char[palabraSeleccionada.length()];
+
+        for (int i = 0; i < letrasAdivinadas.length; i++) {
+            letrasAdivinadas[i] = '_';
+        }
+
+        oportunidades = 5;
+        actualizarPantalla();
+    }
+
+    private void actualizarPantalla() {
+        StringBuilder texto = new StringBuilder();
+        for (char c : letrasAdivinadas) {
+            texto.append(c).append(" ");
+        }
+        jLabel6.setText(texto.toString().trim());
+        jLabel5.setText("(" + oportunidades + ")");
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jLabel6 = new javax.swing.JLabel();
+        BotonAceptarIngresar = new javax.swing.JButton();
+        jLabel5 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setBackground(new java.awt.Color(204, 255, 0));
+        jLabel1.setFont(new java.awt.Font("Arial", 1, 24)); // NOI18N
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("JUEGO AHORCADOS >:D");
+
+        jLabel2.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        jLabel2.setText("Ingrese una letra:");
+
+        jLabel3.setFont(new java.awt.Font("Arial", 1, 15)); // NOI18N
+        jLabel3.setText("PALABRA:");
+
+        jLabel4.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        jLabel4.setText("Oportunidades:");
+
+        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField1ActionPerformed(evt);
+            }
+        });
+
+        jLabel6.setFont(new java.awt.Font("Arial", 1, 15)); // NOI18N
+        jLabel6.setText("_ _ _ _ _");
+
+        BotonAceptarIngresar.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        BotonAceptarIngresar.setText("ACEPTAR");
+        BotonAceptarIngresar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BotonAceptarIngresarActionPerformed(evt);
+            }
+        });
+
+        jLabel5.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        jLabel5.setText("()");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(161, 161, 161)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel3)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 227, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 146, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(BotonAceptarIngresar))
+                    .addComponent(jLabel2)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel4)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLabel5)))
+                .addContainerGap(70, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 325, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(96, 96, 96))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 62, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(41, 41, 41)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(jLabel6))
+                .addGap(54, 54, 54)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(BotonAceptarIngresar))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4)
+                    .addComponent(jLabel5))
+                .addContainerGap(57, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField1ActionPerformed
+
+    private void BotonAceptarIngresarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BotonAceptarIngresarActionPerformed
+        String entrada = jTextField1.getText().toUpperCase();
+        jTextField1.setText("");
+
+        if (entrada.length() != 1 || !Character.isLetter(entrada.charAt(0))) {
+            JOptionPane.showMessageDialog(this, "Ingrese una letra valida.");
+            return;
+        }
+
+        char letra = entrada.charAt(0);
+        boolean letraYaIntentada = false;
+        for (char letraAdivinada : letrasAdivinadas) {
+            if (letraAdivinada == letra) {
+                letraYaIntentada = true;
+                break;
+            }
+        }
+        if (letraYaIntentada) {
+            JOptionPane.showMessageDialog(this, "Ya intentaste esa letra.");
+            return;
+        }
+
+        boolean acierto = false;
+        for (int i = 0; i < palabraSeleccionada.length(); i++) {
+            if (palabraSeleccionada.charAt(i) == letra && letrasAdivinadas[i] == '_') {
+                letrasAdivinadas[i] = letra;
+                acierto = true;
+            }
+        }
+
+        if (!acierto) {
+            oportunidades--;
+        }
+
+        actualizarPantalla();
+
+        if (String.valueOf(letrasAdivinadas).equals(palabraSeleccionada)) {
+            JOptionPane.showMessageDialog(this, "Ganaste :D! La palabra es: " + palabraSeleccionada);
+            this.dispose(); 
+            menuPrincipal.setVisible(true); 
+        } else if (oportunidades == 0) {
+            JOptionPane.showMessageDialog(this, "Perdiste :c! La palabra era: " + palabraSeleccionada);
+            this.dispose(); 
+            menuPrincipal.setVisible(true); 
+        }
+    }//GEN-LAST:event_BotonAceptarIngresarActionPerformed
+
+    public static void main(String args[]) {
+
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton BotonAceptarIngresar;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JTextField jTextField1;
+    // End of variables declaration//GEN-END:variables
 }
---
+++ Laboratorio1Semana1/src/Semana6/Garcia_Jeremy_Menu.java
@@ -0,0 +1,166 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
+package Semana6;
+
+import javax.swing.*;
+
+public class Garcia_Jeremy_Menu extends javax.swing.JFrame {
+
+    public static String[] palabras = {
+    };
+    
+    private Garcia_Jeremy_Ahorcados juegoFrame; // Esta línea es la que debes añadir
+    private Garcia_Jeremy_CambiarPalabras cambiarPalabrasFrame;
+    
+    public Garcia_Jeremy_Menu() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        BotonJugar = new javax.swing.JButton();
+        BotonCambiarP = new javax.swing.JButton();
+        BotonSalirMenu = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setBackground(new java.awt.Color(204, 255, 0));
+        jLabel1.setFont(new java.awt.Font("Arial", 1, 24)); // NOI18N
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("MENU PRINCIPAL AHORCADOS");
+
+        BotonJugar.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        BotonJugar.setText("JUGAR");
+        BotonJugar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BotonJugarActionPerformed(evt);
+            }
+        });
+
+        BotonCambiarP.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        BotonCambiarP.setText("CAMBIAR PALABRAS");
+        BotonCambiarP.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BotonCambiarPActionPerformed(evt);
+            }
+        });
+
+        BotonSalirMenu.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        BotonSalirMenu.setText("SALIR");
+        BotonSalirMenu.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BotonSalirMenuActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(BotonJugar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(BotonCambiarP, javax.swing.GroupLayout.DEFAULT_SIZE, 166, Short.MAX_VALUE)
+                    .addComponent(BotonSalirMenu, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(0, 0, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(93, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addGap(85, 85, 85))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 62, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(32, 32, 32)
+                .addComponent(BotonJugar, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(BotonCambiarP, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(BotonSalirMenu, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(42, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void BotonJugarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BotonJugarActionPerformed
+        this.setVisible(false);
+        if (juegoFrame == null || !juegoFrame.isVisible()) {
+            juegoFrame = new Garcia_Jeremy_Ahorcados(this, palabras); // Pasar 'this' (el menú)
+        }
+        juegoFrame.setVisible(true);
+        juegoFrame.iniciarJuego(); 
+    }//GEN-LAST:event_BotonJugarActionPerformed
+
+    private void BotonCambiarPActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BotonCambiarPActionPerformed
+        this.setVisible(false);
+        new Garcia_Jeremy_CambiarPalabras().setVisible(true);
+    }//GEN-LAST:event_BotonCambiarPActionPerformed
+
+    private void BotonSalirMenuActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BotonSalirMenuActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_BotonSalirMenuActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Garcia_Jeremy_Menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Garcia_Jeremy_Menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Garcia_Jeremy_Menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Garcia_Jeremy_Menu.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Garcia_Jeremy_Menu().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton BotonCambiarP;
+    private javax.swing.JButton BotonJugar;
+    private javax.swing.JButton BotonSalirMenu;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
+    // End of variables declaration//GEN-END:variables
+}
